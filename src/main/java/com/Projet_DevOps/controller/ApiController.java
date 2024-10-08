ce5013d3898e037dd0d7ae8602ca03ff5c8167ad
youssef khalifi <youssefkhalifi767@gmail.com>
2024-10-08 16:05:59 +0100

    remove some code smells

--- src/main/java/com/Projet_DevOps/controller/ApiController.java
+++ src/main/java/com/Projet_DevOps/controller/ApiController.java
@@ -2,7 +2,6 @@ package com.Projet_DevOps.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
